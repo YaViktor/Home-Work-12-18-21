ccd6640f84a99d5d2329cf5cec4645dfdb702975
Viktor Iarushin <95198010+YaViktor@users.noreply.github.com>
2021-12-18 13:33:27 -0800

    programms

---
+++ 1.py
@@ -0,0 +1,17 @@
+# 1.
+# n = int(input())
+# print(n+n)
+# 2.
+# x = 46
+# y = str(input())
+# print(x)
+# print(y*5)
+3.
+x = 5934
+y = [int(y) for y in str(x)]
+print(y)
+4.
+x = str(input("insert your name:"))
+y = str(input("insert your last name:"))
+z = str(input("insert your age:"))
+print(x,y,z)
